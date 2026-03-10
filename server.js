df8646904e8c8221d30ca292d34fc93e1ff4fff0
SwapnaliKhomane <swapnalikhomane44@gmail.com>
2026-03-10 14:59:53 +0530

    add database name

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ const sessionRoutes = require("./routes/sessionRoutes");
 const llmRoutes = require("./routes/llmRoutes");
 
 const app = express();
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/test-api", (req, res) => res.send("Mentor Backend Running "));
+app.get("/", (req, res) => res.send("Mentor Backend Running"));
 
 app.use("/auth", authRoutes);
 app.use("/students", studentRoutes);
@@ -26,4 +27,4 @@ app.use("/bookings", bookingRoutes);
 app.use("/sessions", sessionRoutes);
 app.use("/llm", llmRoutes);
 
-app.listen(process.env.PORT, () => console.log("Server running on port " + process.env.PORT));
+module.exports = app;
